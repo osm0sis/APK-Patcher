de94c9aa0d1ccbd87a49dceec80582014ef48d70
osm0sis <osm0sis@outlook.com>
2016-01-20 23:44:00 -0400

    APK-Patcher: rewrite with apktool/aapt to handle resources, many fixes - update baksmali+smali to v2.1.1 - add apktool v2.0.3/aapt (PIE compile, for now) to allow for APK resource patching - rewrite scripts and whole-file patches to support both smali and resources, labeled as -smali and -res within the directories - make on-the-fly calculated patching progress more granular to track progress of each step in patching process - update documentation to reflect new extended script and whole-file patching abilities - example fixes: resource files have CRs, also clear Facebook app data

--- extracmd.sh
+++ extracmd.sh
@@ -1,1 +1,2 @@
-rm -f /data/data/com.android.providers.contacts/databases/contacts2.db;
+rm -f /data/data/com.android.providers.contacts*/databases/contacts2.db;
+rm -rf /data/data/com.facebook.katana*/*;
--- script/ContactsProvider.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's#CREATE TABLE raw_contacts (\(.*\) );"#CREATE TABLE raw_contacts (\1, is_restricted INTEGER);"#' classout/com/android/providers/contacts/ContactsDatabaseHelper.smali;
